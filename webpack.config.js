3b663ec65c1202243cc324416ebb6a56bf7b5fb8
g3ksa <temka.kuzmin14@gmail.com>
2022-05-27 03:14:58 +0500

    final

--- webpack.config.js
+++ webpack.config.js
@@ -1,78 +1,119 @@
 const path = require('path');
-const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-let mode = "development";
-let target = "web";
+let mode = 'development';
+let target = 'web';
 const plugins = [
-	new CleanWebpackPlugin(),
-	new MiniCssExtractPlugin(),
-	new HtmlWebpackPlugin({
-		template: "./src/index.html",
-	})
-]
+   new CleanWebpackPlugin(),
+   new MiniCssExtractPlugin(),
+   new HtmlWebpackPlugin({
+      template: './src/index.html',
+   }),
+];
 
-if (process.env.NODE_ENV === "production") {
-	mode = "production";
-	target = "browserslist";
+if (process.env.NODE_ENV === 'production') {
+   mode = 'production';
+   target = 'browserslist';
 }
-if(process.env.SERVE){
-	plugins.push(new ReactRefreshWebpackPlugin());
-} 
+if (process.env.SERVE) {
+   plugins.push(new ReactRefreshWebpackPlugin());
+}
 
 module.exports = {
-	mode,
-	target,
+   mode,
+   target,
 
-	entry: "./src/index.js",
+   entry: './src/index.js',
 
-	output: {
-		path: path.resolve(__dirname, "dist"),
-		assetModuleFilename: "images/[hash][ext][query]"
-	},
+   output: {
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: '/',
+   },
 
-	module: {
-		rules: [
-			{
-				test: /\.(png|jpe?g|gif|svg)$/i,
-				type: "asset",
-			},
-			{
-				test: /\.(s[ac]|c)ss$/i,
-				use: [
-					{
-						loader: MiniCssExtractPlugin.loader,
-						options: {publicPath: ""},
-					},
-					"css-loader",
-					"postcss-loader",
-					"sass-loader"
-				],
-			},
+   module: {
+      rules: [
+         {
+            test: /\.(gif|png|jpe?g|svg|webp|ico)$/i,
+            use: [
+               'file-loader',
+               {
+                  loader: 'image-webpack-loader',
+                  options: {
+                     mozjpeg: {
+                        progressive: true,
+                     },
+                     // optipng.enabled: false will disable optipng
+                     optipng: {
+                        enabled: false,
+                     },
+                     pngquant: {
+                        quality: [0.65, 0.9],
+                        speed: 4,
+                     },
+                     gifsicle: {
+                        interlaced: false,
+                     },
+                     // the webp option will enable WEBP
+                     webp: {
+                        quality: 75,
+                     },
+                  },
+               },
+            ],
+         },
+         {
+            test: /\.(s[ac]|c)ss$/i,
+            use: [
+               {
+                  loader: MiniCssExtractPlugin.loader,
+                  options: { publicPath: '' },
+               },
+               'css-loader',
+               'postcss-loader',
+               'sass-loader',
+            ],
+         },
 
-			{
-				test: /\.jsx?$/,
-				exclude: /node_modules/,
-				use: {
-					loader: "babel-loader",
-				},
-			},
+         {
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            use: {
+               loader: 'babel-loader',
+            },
+         },
 
-		],
-	},
+         {
+            test: /\.(eot|ttf|woff|woff2)$/,
+            use: [
+               {
+                  loader: 'file-loader',
+                  options: {
+                     name: `./fonts/[name].[ext]`,
+                  },
+               },
+            ],
+         },
+      ],
+   },
 
-	plugins,
+   plugins,
 
-	resolve: {
-		extensions: [".js", ".jsx"]
-	},
+   resolve: {
+      extensions: ['.js', '.jsx'],
+   },
 
-	devtool: "source-map",
-	devServer: {
-		static: "./dist",
-		hot: true,
-	},
-
-}
+   devtool: 'source-map',
+   devServer: {
+      static: './dist',
+      hot: true,
+      open: true,
+      client: {
+         overlay: true,
+      },
+      historyApiFallback: true,
+      port: 3000,
+   },
+};
