254c76bda755c0e3183cd59757b34273c6ee8beb
g3ksa <temka.kuzmin14@gmail.com>
2022-10-21 00:11:53 +0500

    add template

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import './styles/style.scss'
+import App from './App'
+import { BrowserRouter } from 'react-router-dom'
+
+const root = ReactDOM.createRoot(document.getElementById('wrapper'))
+root.render(
+	<BrowserRouter>
+		<App />
+	</BrowserRouter>
+)
